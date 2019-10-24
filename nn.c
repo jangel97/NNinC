c6d9e18b69881ede7494b2b209a6cb9a50b7d95f
joselillo <jang3l1997@gmail.com>
2019-10-25 00:05:50 +0200

    le he dado la vuelta a los indices, para cambiar la forma de las matrices, ya que generalmente cuando uno esta en la capa L calculando el valor del feed forward, utiliza los valores de relacion con la L-1, de esta manera la i representaria mi neurona actual y la j representaria una neurona en la capa anterior, y el Wij seria el peso que une mi neurona i (capa L) con la neurona j capa (L-1)

--- nn.c
+++ nn.c
@@ -14,13 +14,13 @@ int main(){
         wij[n]= (double**) malloc(sizeof(double**)*layer_sizes[n]);         //creo cada tabla que apuntara a otra tabla con doubles
     }
     for (int n=0 ; n<number_of_layers ; n++){
-        for(int i=0; i<layer_sizes[n]; i++){                                //creamos tabla que guarde tantas posiciones como tablas pueda tener!
-            wij[n][i]= (double*) malloc(sizeof(double*)*layer_sizes[n+1]); //cada posicion de la tabla apunta a otra tabla con sizes[i]+1 elementos
+        for(int i=0; i<layer_sizes[n+1]; i++){                                //creamos tabla que guarde tantas posiciones como tablas pueda tener!
+            wij[n][i]= (double*) malloc(sizeof(double*)*layer_sizes[n]); //cada posicion de la tabla apunta a otra tabla con sizes[i]+1 elementos
         }
     }
     for (int n=0 ; n<number_of_layers; n++){
-        for(int i=0; i<layer_sizes[n] ; i++){                               //para cada una de las tablas reservare espacio para los sizes[i+1] elementos
-            for(int j=0; j<layer_sizes[n+1] ; j++){                          //rellenamos cada uno de esos [i+1] elementos
+        for(int i=0; i<layer_sizes[n+1] ; i++){                               //para cada una de las tablas reservare espacio para los sizes[i+1] elementos
+            for(int j=0; j<layer_sizes[n] ; j++){                          //rellenamos cada uno de esos [i+1] elementos
                 wij[n][i][j]= (double)rand()/RAND_MAX*2.0-1.0;
             }
         }
@@ -29,26 +29,27 @@ int main(){
 /*  JUEGO DE PRUEBAS    
 Miramos entre la capa 0 y la capa 1 la matriz de sinapsis
 */
-    for (int x=0;   x<layer_sizes[0]    ;   x++ ){
-        for (int k=0    ;   k<layer_sizes[1]    ;   k++){
+    for (int x=0;   x<layer_sizes[1]    ;   x++ ){
+        for (int k=0    ;   k<layer_sizes[0]    ;   k++){
             printf("%lf",wij[0][x][k]);                             //para la capa 0, miramos su matriz de transicion de pesos (valor sinapsis), que es de 4x6 (4 neuronas capa 0, 6 neuronas capa 1)
         }   
         printf("\n");
     }
  printf("\n");
+ printf("----------------------------------------------");
   printf("\n");
-/*
-Para imprimir todas las matrices
-*/
+
+//Para imprimir todas las matrices
 for (int n=0 ; n<number_of_layers ; n++){
-        for(int i=0; i<layer_sizes[n] ; i++){
-            for (int j=0; j<layer_sizes[n+1] ; j++){
+        for(int i=0; i<layer_sizes[n+1] ; i++){
+            for (int j=0; j<layer_sizes[n] ; j++){
                 printf("%lf ",(double)wij[n][i][j]);
             }
             printf("\n");
         }
         printf("\n");
     }
+
 
 
 }
